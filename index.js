ebd2fc9f8d6abd9816f0dcc57439bbccc43995c1
E25WMA <177290742+E25WMA@users.noreply.github.com>
2025-04-19 16:23:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-const axios = require('axios');
-
-const CHANNEL_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
-const USER_ID = process.env.LINE_USER_ID;
-
-const sendLineMessage = async () => {
-  const message = {
-    type: 'image',
-    originalContentUrl: 'https://www.tmd.go.th/assets/tmd/img/weather/daily/small/thailand_today.png',
-    previewImageUrl: 'https://www.tmd.go.th/assets/tmd/img/weather/daily/small/thailand_today.png',
-  };
-
-  await axios.post('https://api.line.me/v2/bot/message/push', {
-    to: USER_ID,
-    messages: [message],
-  }, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`,
-    },
-  });
-
-  console.log("✅ ส่งข้อความพร้อมรูปภาพสำเร็จ");
+const message = {
+  type: 'image',
+  originalContentUrl: 'https://www.tmd.go.th/assets/tmd/img/weather/daily/small/thailand_today.png',
+  previewImageUrl: 'https://www.tmd.go.th/assets/tmd/img/weather/daily/small/thailand_today.png',
 };
-
-sendLineMessage();
