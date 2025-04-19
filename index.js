8f8f6fe97734171cc96a040c71d0390967f2747f
E25WMA <177290742+E25WMA@users.noreply.github.com>
2025-04-19 16:17:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,26 @@ const CHANNEL_ACCESS_TOKEN = process.env.LINE_ACCESS_TOKEN;
 const USER_ID = process.env.LINE_USER_ID;
 
 const getMessage = async () => {
-  const image = 'https://www.tmd.go.th/images/radar/loop_radar_north_east.png';
-  const message = '📍 พยากรณ์อากาศวันนี้\nดูภาพเรดาร์:';
   return {
     type: 'image',
-    originalContentUrl: image,
-    previewImageUrl: image
+    originalContentUrl: 'https://www.tmd.go.th/assets/tmd/img/weather/daily/small/thailand_today.png',
+    previewImageUrl: 'https://www.tmd.go.th/assets/tmd/img/weather/daily/small/thailand_today.png',
   };
 };
 
 const sendLineMessage = async () => {
   const message = await getMessage();
 
-  await axios.post(
-    'https://api.line.me/v2/bot/message/push',
-    {
-      to: USER_ID,
-      messages: [message]
+  await axios.post('https://api.line.me/v2/bot/message/push', {
+    to: USER_ID,
+    messages: [message],
+  }, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`,
     },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${CHANNEL_ACCESS_TOKEN}`
-      }
-    }
-  );
+  });
 };
 
 sendLineMessage();
+
